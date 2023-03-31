1c4ce182d0349039940e82bb84d8046db650b977
TheAxumite <100012855+TheAxumite@users.noreply.github.com>
2023-03-31 16:40:39 -0400

    Update

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("load_posts/<str:load>", views.load_posts, name="load_posts"),
     path("create_post", views.create_post, name="create_post"),
-    path("like_post/<int:post_id>", views.like_post, name="like_post")
+    path("like_post/<int:post_id>", views.like_post, name="like_post"),
+    path("follow/<str:username>", views.follow, name="follow"),
+    path("load_page/<str:load>", views.load_page, name="load_page")
     
     
 
